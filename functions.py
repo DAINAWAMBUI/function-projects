1ce1c4713e8b5cf52837f16c2422153bafb48e91
DAINAWAMBUI <61407935+DAINAWAMBUI@users.noreply.github.com>
2021-07-21 12:36:40 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,32 @@
+def add(a,b):
+    return a+b
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+z=add(x,y)
+print("sum of ",x,"and",y,"is",z)
+def sub(a,b):
+    return a-b
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+z=sub(x,y)
+print("subtraction of ",x,"and",y,"is",z)
+def div(a,b):
+    return a/b
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+z=div(x,y)
+print("division of ",x,"and",y,"is",z)
+def mult(a,b):
+    return a*b
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+z=mult(x,y)
+print("multiplication of ",x,"and",y,"is",z)
+def square(a,b):
+    return a**b
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+z=square(x,y)
+print("square of ",x,"and",y,"is",z)
+
+
